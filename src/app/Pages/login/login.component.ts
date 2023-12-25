eee8abc583c9deb6a2e5adae6719c4861e3f0c2f
Takondwa Kapyola <105532335+takondwamw@users.noreply.github.com>
2023-12-25 12:22:15 +0200

    added login

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormGroup,FormBuilder, Validators } from '@angular/forms';
+import { environment } from 'src/environment/environment';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  
+  loginForm!: FormGroup;
+  sMessage!:string;
+  eMessage!: string;
 
+
+  constructor(private _http: HttpClient, private _fb: FormBuilder){
+
+    this.loginForm = this._fb.group({
+      userName: ['',Validators.required],
+      Password: ['',Validators.required],
+    })
+  }
+
+  login(){
+    if(this.loginForm.valid && this.loginForm.value !== null){
+       this._http.post(`${environment._baseUrl}/Login`,this.loginForm.value).subscribe({
+        next: (resp:any) =>{
+            if(resp.result == true){
+              // alert(resp.message);
+              return this.sMessage = resp.message;
+
+            }else{
+              this.sMessage = resp.data;
+            }
+       
+        },
+        error: (error:any)=>{
+          console.log(error);
+          return this.eMessage = error;
+        }
+       })
+    }
+  }
 }
