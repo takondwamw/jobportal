0befddc48ddef87a42663feb901ea86cae83b85a
Takondwa Kapyola <105532335+takondwamw@users.noreply.github.com>
2023-12-26 11:56:10 +0200

    updates

--- src/app/Pages/jobs/jobs.component.ts
+++ src/app/Pages/jobs/jobs.component.ts
@@ -12,11 +12,15 @@ export class JobsComponent implements OnInit {
   activeJobs!: Job[];
   allJobs!: Job[];
   jobs: Job[] = [];
+  isLoading!:boolean;
 
   constructor(private _jobService: JobsService ){
+    this.isLoading = true;
     this._jobService.Activejobs().subscribe({
       next: (resp: any)=>{
-        console.log(resp.data);
+        this.isLoading =false;
+        // console.log(resp.data);
+
         this.activeJobs = resp.data;
       },
       error: (error:any)=>{
@@ -27,7 +31,8 @@ export class JobsComponent implements OnInit {
 
     this._jobService.allJobs().subscribe({
       next: (resp: any)=>{
-        console.log(resp.data);
+        // console.log(resp.data);
+        this.isLoading =false;
         this.allJobs = resp.data;
         this.jobs = resp.data;
       },
@@ -38,12 +43,11 @@ export class JobsComponent implements OnInit {
 
   }
 ngOnInit(): void {
+  // this.isLoading = true;
   this.jobs = this.allJobs;
 }
 byActiveJobs(){
   return this.jobs = this.activeJobs;
 }
-
-
 
 }
